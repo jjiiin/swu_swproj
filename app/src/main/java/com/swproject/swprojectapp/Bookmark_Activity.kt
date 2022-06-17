7ff76355660dd1d083d9c1f3ba20f806b5d9991d
whk06061 <63198157+whk06061@users.noreply.github.com>
2022-06-18 01:05:53 +0900

    북마크 URL 유효성 검사기능 추가

--- app/src/main/java/com/swproject/swprojectapp/Bookmark_Activity.kt
+++ app/src/main/java/com/swproject/swprojectapp/Bookmark_Activity.kt
@@ -3,6 +3,7 @@ package com.swproject.swprojectapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebResourceRequest
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,6 +15,9 @@ import com.swproject.swprojectapp.dataModel.NoticeData
 import com.swproject.swprojectapp.utils.Auth
 import com.swproject.swprojectapp.utils.FBRef
 import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class Bookmark_Activity : AppCompatActivity() {
     val noticeDatas = mutableListOf<NoticeData>()
@@ -39,13 +43,12 @@ class Bookmark_Activity : AppCompatActivity() {
                         if (data.getValue() != null) {
                             try{
                                 val noticeData = data.getValue(NoticeData::class.java)
-                                noticeDatas.add(noticeData!!)
+                                checkUrl(noticeData!!)
                             } catch (e:Exception){
                                 continue
                             }
                         }
                     }
-                    rvAdapter.notifyDataSetChanged()
                 }
 
                 override fun onCancelled(error: DatabaseError) {
@@ -53,5 +56,33 @@ class Bookmark_Activity : AppCompatActivity() {
                 }
 
             })
+    }
+
+    //유효한 URL인지 확인
+    fun checkUrl(data:NoticeData){
+        //스레드 생성
+        thread{
+            try{
+                HttpURLConnection.setFollowRedirects(false)
+                val con : HttpURLConnection = URL(data.link).openConnection() as HttpURLConnection
+                con.requestMethod = "HEAD"
+                if(con.responseCode == HttpURLConnection.HTTP_OK){
+                    //유효한 주소인경우
+                    noticeDatas.add(data)
+                    //UI에 접근할 수 있음
+                    this.runOnUiThread {
+                        Log.d("url체크", data.toString())
+                        rvAdapter.notifyDataSetChanged()
+                    }
+                } else{
+                    //없는 주소인 경우 북마크에서 삭제
+                    FBRef.bookmarkRef.child(Auth.current_uid).child(data.bookmarkKey).removeValue()
+                }
+            } catch (e:Exception){
+                Log.d("url체크", e.toString())
+                //없는 주소인 경우 북마크에서 삭제
+                FBRef.bookmarkRef.child(Auth.current_uid).child(data.bookmarkKey).removeValue()
+            }
+        }.join()
     }
 }
